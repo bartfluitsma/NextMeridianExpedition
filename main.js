a7cbdb6e7d9506d6bf6a980999eb64f2bbc87aa0
Bart Fluitsmaa <bartfluitsma@gmail.com>
2021-09-07 14:43:19 +0200

    JS file update + google tag

--- main.js
+++ main.js
@@ -17,46 +17,7 @@ $('.closeMap').on('click', () => {
 
 $('.popupMap').on('click', () => { 
   $('.popupMapShow').addClass('popupMap').removeClass('popupMapShow');
-})
-
-// COUNTDOWN
-// Set the date we're counting down to
-var countDownDate = new Date("Apr 18, 2022 08:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="timer"
-  document.getElementById("timerDays").innerHTML = days;
-
-  // Output the result in an element with id="timer"
-  document.getElementById("timerHours").innerHTML = hours;
-
-  // Output the result in an element with id="timer"
-  document.getElementById("timerMinutes").innerHTML = minutes;
-
-  // Output the result in an element with id="timer"
-  document.getElementById("timerSeconds").innerHTML =  seconds ;
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
+});
 
 // slidejs
 if ($(window).width() < 600) {
